4e31d5e467a4607c50d1c73e1a6aaf59dfe238bd
Кабидоллаев Гашиых <gashiykh@bk.ru>
2024-07-11 12:00:30 +0500

    Зарегал модели в админку

---
+++ mysite/webapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from webapp.models import Task, Status, Type
+
+admin.site.register(Status)
+admin.site.register(Task)
+admin.site.register(Type)

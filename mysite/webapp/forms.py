bfbf27015bce99f672bf87f3d35aa7a04c42517c
Кабидоллаев Гашиых <gashiykh@bk.ru>
2024-07-11 11:59:38 +0500

    Добавил формы

---
+++ mysite/webapp/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.forms import widgets
+from .models import Task, Status, Type
+from webapp import models
+
+class TaskForm(forms.Form):
+    title = forms.CharField(
+        max_length=100,
+        required=True,
+        label='Action'
+        )
+    description = forms.CharField(
+        max_length=3000,
+        required=True,
+        label='Description',
+        widget=widgets.Textarea
+        )
+    status = forms.ModelChoiceField(
+        queryset=Status.objects.all(),
+        required=True,
+        label='Status'
+        )
+    types = forms.ModelMultipleChoiceField(
+        queryset=models.Type.objects.all(),
+        required=True,
+        label='Type',
+        widget=widgets.CheckboxSelectMultiple)
+
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'status', 'types']
+

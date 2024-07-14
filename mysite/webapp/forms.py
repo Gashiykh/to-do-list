35f93a5727e048a1dd8690a3a67774b5278c9f8f
Кабидоллаев Гашиых <gashiykh@bk.ru>
2024-07-14 18:04:21 +0500

    Добавил 2кастомных валидатора и обновил на ModelForm

--- mysite/webapp/forms.py
+++ mysite/webapp/forms.py
@@ -1,32 +1,72 @@
 from django import forms
 from django.forms import widgets
-from .models import Task, Status, Type
-from webapp import models
+from webapp.models import Task, Status, Type
+from django.core import validators
+from django.utils.deconstruct import deconstructible
 
-class TaskForm(forms.Form):
+
+@deconstructible
+class MinLengthValidator(validators.BaseValidator):
+    message = 'min length must be 10'
+    code = 'short_text'
+
+    def compare(self, value, limit):
+        return value < limit
+
+    def clean(self, value):
+        return len(value)
+    
+class MaxLengthValidator(validators.BaseValidator):
+    message = 'max length must be 50'
+    code = 'long_text'
+
+    def compare(self, value, limit):
+        return value > limit
+
+    def clean(self, value):
+        return len(value)
+
+class TaskFormOld(forms.Form):
     title = forms.CharField(
         max_length=100,
         required=True,
-        label='Action'
-        )
+        label='Action',
+        validators=[
+            MinLengthValidator(10),
+            MaxLengthValidator(50),
+        ],
+    )
     description = forms.CharField(
         max_length=3000,
         required=False,
         label='Description',
         widget=widgets.Textarea
-        )
+    )
     status = forms.ModelChoiceField(
         queryset=Status.objects.all(),
         required=True,
         label='Status'
-        )
+    )
     types = forms.ModelMultipleChoiceField(
-        queryset=models.Type.objects.all(),
+        queryset=Type.objects.all(),
         required=True,
         label='Type',
-        widget=widgets.CheckboxSelectMultiple)
+        widget=widgets.CheckboxSelectMultiple
+    )
+
+
+class TaskForm(forms.ModelForm):
+    title = forms.CharField(
+        max_length=100,
+        required=True,
+        label='Action',
+        validators=[
+            MinLengthValidator(10),
+            MaxLengthValidator(50),
+        ],
+    )
+
 
     class Meta:
         model = Task
         fields = ['title', 'description', 'status', 'types']
-

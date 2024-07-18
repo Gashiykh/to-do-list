5a4424899cae625f4bb770d595695ce0beff5114
Кабидоллаев Гашиых <gashiykh@bk.ru>
2024-07-18 17:33:53 +0500

    Загрузил остальные файлы

--- mysite/webapp/views/__init__.py
+++ mysite/webapp/views/__init__.py
@@ -1,5 +1,6 @@
 from webapp.views.task import IndexView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView
 from django.views import generic
+from webapp.views.project import ProjectListView, ProjectDetailView, ProjectCreateView
 
 class IndexRedirectView(generic.RedirectView):
-    pattern_name = 'home'
+    pattern_name = 'projects'

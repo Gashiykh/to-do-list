cbec49df5b2c61dc2ebc2b27d5ea3bae29a26559
Кабидоллаев Гашиых <gashiykh@bk.ru>
2024-07-18 17:29:22 +0500

    Написал представления для проекта

---
+++ mysite/webapp/views/project.py
@@ -0,0 +1,34 @@
+from typing import Any
+from django.urls import reverse
+from django.views import generic
+from webapp.models import Project
+from webapp.forms import ProjectForm
+
+
+class ProjectListView(generic.ListView):
+    template_name = 'projects/list.html'
+    model = Project 
+    context_object_name = 'projects'
+    ordering = ['-started_at']
+
+
+class ProjectDetailView(generic.DeleteView):
+    template_name = 'projects/detail.html'
+    model = Project
+    pk_url_kwarg = 'id'
+
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        project = self.object
+        tasks = project.tasks.order_by('-created_at')
+        context['tasks'] = tasks
+        return context
+    
+
+class ProjectCreateView(generic.CreateView):
+    template_name = 'projects/project.html'
+    model = Project
+    form_class = ProjectForm 
+    
+    def get_success_url(self) -> str:
+        return reverse('projects_detail', kwargs={'id': self.object.id})

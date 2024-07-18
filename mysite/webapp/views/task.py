55448f59f4d3e4dbbc4fc9944e70e4c89d6753e1
Кабидоллаев Гашиых <gashiykh@bk.ru>
2024-07-18 17:29:56 +0500

    Обновил представления для тасков

--- mysite/webapp/views/task.py
+++ mysite/webapp/views/task.py
@@ -1,98 +1,115 @@
 from typing import Any
-from django.http import HttpRequest
+from urllib.parse import urlencode
+from django.http import Http404, HttpResponseRedirect
 from django.http.response import HttpResponse as HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.views import generic
-from webapp.models import Task
-from webapp.forms import TaskForm
+from webapp.models import Task, Project
+from webapp.forms import TaskForm, SearchForm
+from django.db.models import Q
 
 
-class IndexView(generic.TemplateView):
+class IndexView(generic.ListView):
     template_name = 'tasks/list.html'
+    model = Task
+    context_object_name = 'tasks'
+    ordering = ['-created_at']
+    paginate_by = 10
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tasks'] = Task.objects.all()
-
-        return context
-
- 
-class TaskDetailView(generic.TemplateView):
-    template_name = 'tasks/detail.html'
-
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        context['task'] = get_object_or_404(Task, id=self.kwargs.get('id'))
-
-        return context
     
 
-class TaskCreateView(generic.FormView):
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+
+        return super().get(request, *args, **kwargs)
+    
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form'] = self.form
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
+
+        if not context['tasks']:
+            context['message'] = "Задачи не найдены."
+        
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.search_value:
+            query = Q(title__icontains=self.search_value) | Q(description__icontains=self.search_value)
+            queryset = queryset.filter(query)
+        queryset = queryset.filter(is_deleted=False)
+        return queryset
+
+
+    def get_search_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+        return None
+
+
+
+class TaskDetailView(generic.DetailView):
+    pk_url_kwarg = 'id'
+    model = Task
+    template_name = 'tasks/detail.html'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset=queryset)
+        if obj.is_deleted:
+            raise Http404("Задача не найдена")
+        return obj
+    
+
+class TaskCreateView(generic.CreateView):
 
     template_name = 'tasks/task.html'
     form_class = TaskForm
+    model = Task
 
-    
     def form_valid(self, form):
-        types = form.cleaned_data.pop('types')
+        project_id = self.kwargs.get('project_id')
+        task = form.save(commit=False)
+        task.project_id = project_id
+        task.save()
+        types = form.cleaned_data['types']
+        task.types.set(types)
 
-        self.task = Task.objects.create(
-            title=form.cleaned_data.get('title'),
-            description=form.cleaned_data.get('description'),
-            status=form.cleaned_data.get('status'),
-        )
-
-        self.task.types.set(types)
-
-
-        return super().form_valid(form)
-    
-    def get_success_url(self):
-        return reverse('tasks_detail',  kwargs={'id': self.task.id})
+        return redirect('projects_detail', id=project_id)
 
 
-class TaskUpdateView(generic.FormView):
+class TaskUpdateView(generic.UpdateView):
         
     template_name = 'tasks/task.html'
     form_class = TaskForm
+    model = Task
+    context_key = 'task'
+    pk_url_kwarg = 'id'
 
-    def dispatch(self, request, *args, **kwargs):
-        self.task = self.get_object()
-        return super().dispatch(request, *args, **kwargs)
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['task'] = self.task
-
-        return context
-    
-    def get_initial(self):
-        initial = {}
-        for key in 'title', 'description', 'status':
-            initial[key] = getattr(self.task, key)
-        initial['types'] = self.task.types.all()
-        return initial
-
-    def form_valid(self, form):
-        data = form.cleaned_data
-
-        self.task.title = data.get('title')
-        self.task.description = data.get('description')
-        self.task.status = data.get('status')
-        self.task.types.set(data.get('types'))
-        self.task.save()
-
-        return super().form_valid(form)
+    def get_success_url(self):
+        return reverse('projects_detail', kwargs={'id': self.object.project.id})
+ 
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    pk_url_kwarg = 'id'
     
     def get_success_url(self):
-        return reverse('tasks_detail', kwargs={'id': self.task.id})
-
-    def get_object(self):
-        return get_object_or_404(Task, id=self.kwargs.get('id'))
-
- 
-class TaskDeleteView(generic.View):
-    def post(self, request, id):
-        task = get_object_or_404(Task, id=id).delete()
-        return redirect('home')
+        return reverse('projects_detail', kwargs={'id': self.object.project.id})
+    
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.is_deleted = True  
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+    
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset=queryset)
+        if obj.is_deleted:
+            raise Http404("Задача не найдена")
+        return obj

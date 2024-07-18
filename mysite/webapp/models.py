64f0b4afb9cf0576640379b9196455b27354f98f
Кабидоллаев Гашиых <gashiykh@bk.ru>
2024-07-18 17:26:49 +0500

    Добавил модель проекта

--- mysite/webapp/models.py
+++ mysite/webapp/models.py
@@ -25,7 +25,8 @@ class Task(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата обновления')
     types = models.ManyToManyField('webapp.Type', related_name='tasks', verbose_name='Типы', through='webapp.TaskType')
     status = models.ForeignKey('webapp.Status', related_name='tasks', on_delete=models.RESTRICT, verbose_name='Статус')
-    
+    project = models.ForeignKey('webapp.Project', related_name='tasks', on_delete=models.CASCADE, verbose_name='Проект')
+    is_deleted = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.title
@@ -37,4 +38,14 @@ class TaskType(models.Model):
 
     def __str__(self) -> str:
         return f'{self.task} - {self.type}'
+    
+
+class Project(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название проекта')
+    description = models.TextField(max_length=500, verbose_name='Описание проекта')
+    started_at = models.DateField(verbose_name='Дата начала') 
+    ended_at = models.DateField(null=True, blank=True, verbose_name='Дата окончания')
+
+    def __str__(self) -> str:
+        return self.title
 

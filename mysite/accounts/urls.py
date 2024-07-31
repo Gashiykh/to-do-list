5fc70c6a4af0ebb41c24cc682269e26b36e6e956
Кабидоллаев Гашиых <gashiykh@bk.ru>
2024-07-31 23:37:59 +0500

    Добавил ост. файлы

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from accounts import views
-from django.contrib.auth import views
+from django.contrib.auth import views as django_views
+from accounts import views
 
 urlpatterns = [
-    path('login', views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout', views.LogoutView.as_view(), name='logout')
+    path(
+        'login',
+        django_views.LoginView.as_view(
+            template_name='accounts/login.html'
+            ), name='login'
+        ),
+    path('logout', django_views.LogoutView.as_view(), name='logout'),
+    path('register', views.RegisterView.as_view(), name='register')
+
 ]

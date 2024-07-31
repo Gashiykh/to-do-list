2a3d2d7c3402a53a374f872042dce2ff1d5ff89c
Кабидоллаев Гашиых <gashiykh@bk.ru>
2024-07-31 23:36:50 +0500

    Добавил представление регистрации

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import redirect, render 
+from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
+from django.views import generic
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from accounts.forms import MyUserCreationForm
 
 # Create your views here.
 
@@ -24,3 +29,24 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('login')
+
+
+class RegisterView(generic.CreateView):
+    template_name = 'accounts/register.html'
+    model = get_user_model()
+    form_class = MyUserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.POST.get('next')
+        
+        if not next_url:
+            next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = reverse('home')
+
+        return next_url

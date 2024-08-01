b345de50e71cea92bf17b0285223e54fb31efb27
Кабидоллаев Гашиых <gashiykh@bk.ru>
2024-08-01 16:22:19 +0500

    Зарегал в админку пользователей

--- mysite/accounts/admin.py
+++ mysite/accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
+from accounts import models
 # Register your models here.
+
+
+class ProfileInline(admin.StackedInline):
+    model = models.Profile
+    fields = ('avatar', 'git_link', 'description')
+
+
+@admin.register(get_user_model())
+class UserAdmin_(UserAdmin):
+    model = get_user_model()
+    inlines = [ProfileInline]

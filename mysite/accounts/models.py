0d63758c676c79f80e36712b1fecf5a745151885
Кабидоллаев Гашиых <gashiykh@bk.ru>
2024-08-01 16:22:57 +0500

    Добавил связь 1к1

--- mysite/accounts/models.py
+++ mysite/accounts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import  AbstractUser
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class User(AbstractUser):
@@ -10,6 +11,7 @@ class Profile(models.Model):
     avatar = models.ImageField(blank=True, null=True, upload_to='avatars', verbose_name='Аватар')
     git_link = models.URLField(blank=True, null=True, verbose_name='Ссылка на гит')
     description = models.TextField(null=True, blank=True, verbose_name='О себе')
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, verbose_name='Пользователь')
 
     class Meta:
         verbose_name = 'Профиль'
